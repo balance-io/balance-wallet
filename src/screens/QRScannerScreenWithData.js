d15ba11bc41bace2a84411796f999244775b70ec
Michael Demarais <m.demarais@gmail.com>
2018-09-30 15:05:31 -0400

    Cleanup QRScanner screens + components

--- src/screens/QRScannerScreenWithData.js
+++ src/screens/QRScannerScreenWithData.js
@@ -1,54 +1,41 @@
-import lang from 'i18n-js';
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Alert } from 'react-native';
-import firebase from 'react-native-firebase';
-import { compose } from 'recompact';
-import { withAccountAddress, withWalletConnectors } from '../hoc';
-import { walletConnectInit } from '../model/walletconnect';
-import QRScannerScreen from './QRScannerScreen';
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { Alert } from "react-native";
+import firebase from "react-native-firebase";
+import { compose } from "recompact";
+import { withAccountAddress, withWalletConnectors } from "../hoc";
+import { walletConnectInit } from "../model/walletconnect";
+import QRScannerScreen from "./QRScannerScreen";
 
 class QRScannerScreenWithData extends PureComponent {
   static propTypes = {
     accountAddress: PropTypes.string,
-    addWalletConnector: PropTypes.func,
     isScreenActive: PropTypes.bool,
-    navigation: PropTypes.object,
-  }
+    navigation: PropTypes.object
+  };
 
-  handlePressBackButton = () => this.props.navigation.push('WalletScreen')
+  handlePressBackButton = () => this.props.navigation.goBack();
 
-  handleSuccess = async (event) => {
-    const { accountAddress, addWalletConnector, navigation } = this.props;
-    const data = event.data;
+  handleSuccess = async event => {
+    const { accountAddress, navigation } = this.props;
+    const data = JSON.parse(event.data);
 
-    if (data) {
+    if (data.domain && data.sessionId && data.sharedKey && data.dappName) {
       try {
-        const walletConnector = await walletConnectInit(accountAddress, data);
-        addWalletConnector(walletConnector);
-        const enabled = await firebase.messaging().hasPermission();
-        if (!enabled) {
-          try {
-            Alert.alert(
-              lang.t('wallet.push_notifications.please_enable_title'),
-              lang.t('wallet.push_notifications.please_enable_body'),
-              [
-                {text: 'Okay', onPress: async () => await firebase.messaging().requestPermission()},
-                {text: 'Dismiss', onPress: () => console.log('Push notification dismissed'), style: 'cancel'}
-              ],
-              { cancelable: false }
-            );
-          } catch (error) {
-            console.log('user has rejected notifications');
-          }
-        }
-        navigation.navigate('WalletScreen');
+        await walletConnectInit(
+          accountAddress,
+          data.domain,
+          data.sessionId,
+          data.sharedKey,
+          data.dappName
+        );
+        navigation.navigate("WalletScreen");
       } catch (error) {
-        AlertIOS.alert(lang.t('wallet.wallet_connect.error'), error);
-        console.log('error initializing wallet connect', error);
+        AlertIOS.alert(lang.t("wallet.wallet_connect.error"), error);
+        console.log("error initializing wallet connect", error);
       }
     }
-  }
+  };
 
   render = () => (
     <QRScannerScreen
@@ -56,10 +43,10 @@ class QRScannerScreenWithData extends PureComponent {
       onPressBackButton={this.handlePressBackButton}
       onSuccess={this.handleSuccess}
     />
-  )
+  );
 }
 
 export default compose(
   withAccountAddress,
-  withWalletConnectors,
+  withWalletConnectors
 )(QRScannerScreenWithData);
