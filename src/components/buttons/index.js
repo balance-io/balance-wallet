0cbbda4cc6bdaff7f21f480d8a471f28a8d05a5d
Logan Bernard <bernard.logan4@gmail.com>
2018-10-11 15:18:49 -0400

    remove ButtonRow

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,4 +1,3 @@
 export { default as BlockButton } from "./BlockButton";
 export { default as Button } from "./Button";
-export { default as ButtonRow } from "./ButtonRow";
 export { default as ButtonPressAnimation } from "./ButtonPressAnimation";
